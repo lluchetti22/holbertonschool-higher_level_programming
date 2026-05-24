5bfdef6ae87cc8aa5387f5cf9dae3d6088a4547d
Lachlan Luchetti <lachlan.luchetti@outlook.com>
2026-05-24 05:54:03 +0000

    python-classes/

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square.
+    Args:
+        size: The size of the squuare
+    """
+    self.__size = size
+    
