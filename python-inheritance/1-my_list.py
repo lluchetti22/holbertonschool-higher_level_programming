c102ef0cd9afe8a6d54d10c9b8d70d6885252bea
d <d>
2026-05-28 20:57:31 +1000

    python-inheritance/

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains the print_sorted function"""
+
+class MyList(list):
+    """A subclass of list with a custom print_sorted method."""
+
+def print_sorted(self):
+    """prints the list sorted in ascending order."""
+    print(sorted(self))
