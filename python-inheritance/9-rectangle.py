2068d780f926679166b4feea9e3498639ccb0446
d <d>
2026-05-28 21:47:36 +1000

    python-inheritance/

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """Module that defines the Rectangle class."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
@@ -18,11 +17,11 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         """Returns the area of the rectangle."""
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Returns a strong description of the rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
