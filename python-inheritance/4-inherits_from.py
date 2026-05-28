2064d7dd0be3af797145498d636befefe6718795
d <d>
2026-05-28 21:22:04 +1000

    python-inheritance/

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """Returns True if obj is an instance of a subclass of a_class.
-    
+
     Otherwise returns False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
