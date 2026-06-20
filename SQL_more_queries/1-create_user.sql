ead66776de4050b9370f55ecd8e04be53d186683
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 15:42:27 +1000

    SQL_more_queries/

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
--- Creates the user safely if they do not exist
+-- Creates the MySQL server user user_0d_1 with all privileges and a password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grants global privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Reloads the grant tables to apply changes immediately
 FLUSH PRIVILEGES;
