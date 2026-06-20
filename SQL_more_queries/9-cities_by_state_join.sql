3018d8a13356ab640d7b492e4b2e5fe2f0fdab5f
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 17:16:04 +1000

    SQL_more_queries/

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
--- Lists all cities with their state names from the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-ON cities.states_id = states.id
+-- Lists all cities in the hbtn_0d_usa database with their corresponding state names.
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+JOIN states ON cities.state_id = states.id 
 ORDER BY cities.id ASC;
