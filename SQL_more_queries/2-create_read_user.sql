74b6ae6dd7a4d195636ba8c14e488303d1f2f036
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 16:21:39 +1000

    SQL_more_queries/

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,11 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2 with SELECT privilege
+-- Create the database if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not already exist and set the password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECVT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Grant only SELECT privilege on the specific database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to ensure that the changes take effect immediately
 FLUSH PRIVILEGES;
