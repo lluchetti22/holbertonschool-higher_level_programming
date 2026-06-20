ba52de1635fab208675110bef3be8cb1f92b3720
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 16:54:36 +1000

    SQL_more_queries/

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Creates the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switches to the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Creates the table cities if it does not exist
+CREATE TABKE UF BIT EXISTS cities (
+    id INT UNIQUUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
