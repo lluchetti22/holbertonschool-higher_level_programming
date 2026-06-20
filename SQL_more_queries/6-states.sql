e011904005cb8e75bb1ec185b6e05f2a907c2941
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 16:50:39 +1000

    SQL_more_queries/

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@ USE hbtn_0d_usa;
 
 -- Creates the table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE AUTO_INCREMENT NBOT NULL PRIMARY KEY,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
