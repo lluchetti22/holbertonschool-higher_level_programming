446dbb5746bbd634efab0dff739214b08c2fd59b
Lachlan <lachlan.luchetti@outlook.com>
2026-06-20 16:25:39 +1000

    SQL_more_queries/

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- Create the table id_not_null if it does not already exist
-CREATE TABLE IF NOT EXISTS id_not_nuull (
+-- Creates the table id_not_null with specific column constraints
+CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
