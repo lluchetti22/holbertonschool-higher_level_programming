66772a7f15433d26ce8cac51930f5d51108f5477
d <d>
2026-06-28 12:06:52 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv,
-        passwd=sys.argv,
-        db=sys.argv
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     cursor = db.cursor()
     cursor.execute(
         "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            sys.argv
+            sys.argv[4]
         )
     )
     query_rows = cursor.fetchall()
