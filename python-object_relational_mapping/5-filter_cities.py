3824ec7dfe050c1dbf33d1f356ffa8240eca02bc
d <d>
2026-06-28 12:37:12 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""Lists all cities of a user-specified state from hbtn_0e_4_usa safely"""
-import sys
+"""Lists all cities of a given state from hbtn_0e_4_usa"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv,
-        passwd=sys.argv,
-        db=sys.argv
-    )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute(
         "SELECT cities.name FROM cities "
         "JOIN states ON cities.state_id = states.id "
         "WHERE states.name = %s ORDER BY cities.id ASC",
-        (sys.argv,)
+        (sys.argv[4],)
     )
-    query_rows = cursor.fetchall()
-    
-    # Correctly isolate the full city string from each database tuple row
-    cities = [row for row in query_rows]
-    
-    print(", ".join(cities))
-    
+    print(", ".join([row[0] for row in cursor.fetchall()]))
     cursor.close()
     db.close()
+    
