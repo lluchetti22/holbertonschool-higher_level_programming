e052862d3734e79983b668942d290803a55c9ac3
d <d>
2026-06-28 12:26:27 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=sys.argv,
+        passwd=sys.argv,
+        db=sys.argv
     )
     cursor = db.cursor()
     query = (
@@ -17,11 +17,11 @@ if __name__ == "__main__":
         "INNER JOIN states ON cities.state_id = states.id "
         "WHERE states.name = %s ORDER BY cities.id ASC"
     )
-    cursor.execute(query, (sys.argv[4],))
+    cursor.execute(query, (sys.argv,))
     query_rows = cursor.fetchall()
     
     # Correctly isolate the first index element from the database tuples
-    cities = [row[0] for row in query_rows]
+    cities = [row for row in query_rows]
     
     print(", ".join(cities))
     
