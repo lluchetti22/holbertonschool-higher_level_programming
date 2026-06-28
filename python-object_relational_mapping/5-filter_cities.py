19a3376a8919654953cea5e749796953160ace0d
d <d>
2026-06-28 12:30:08 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Lists all cities of a user-specified state from hbtn_0e_4_usa safely"""
-import sys
+"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cursor = db.cursor()
-    cursor.execute(
-        "SELECT cities.name FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s ORDER BY cities.id ASC",
-        (sys.argv[4],)
-    )
-    query_rows = cursor.fetchall()
-    
-    # Use explicit tuple unpacking to safely isolate the full city string names
-    cities = []
-    for (city_name,) in query_rows:
-        cities.append(city_name)
-        
-    print(", ".join(cities))
-    
-    cursor.close()
-    db.close()
+    username, password, db, state = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db)
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (state,))
+    print(", ".join(row[0] for row in cur.fetchall()))
+    cur.close()
+    conn.close()
