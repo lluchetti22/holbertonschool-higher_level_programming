b7fdccf546d59fe9ce1a05c3ba27494371374ca1
d <d>
2026-06-28 12:34:39 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
-import MySQLdb
+"""
+Lists all cities of a user-specified state from the database hbtn_0e_4_usa.
+Safe from MySQL injection.
+"""
 import sys
-
+import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute(
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    
+    # Selecting purely cities.name matching the input parameter
+    cursor.execute(
         "SELECT cities.name FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC",
+        "WHERE states.name = %s ORDER BY cities.id ASC",
         (sys.argv[4],)
     )
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    cur.close()
-    conn.close()
+    
+    query_rows = cursor.fetchall()
+    
+    # Using a clean list comprehension with structural index extraction
+    cities = [row[0] for row in query_rows]
+    
+    # This guarantees a blank line is printed even if the cities list is empty
+    print(", ".join(cities))
+    
+    cursor.close()
+    db.close()
