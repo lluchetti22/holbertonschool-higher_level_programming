62423ab7d0ec3cf4ed813c307991acee0ffa8521
d <d>
2026-06-28 12:13:09 +1000

    python-object_relational_mapping/

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,15 +7,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv,
-        passwd=sys.argv,
-        db=sys.argv
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT cities.id, cities.name, states.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
+        "SELECT cities.id, cities.name, states.name FROM cities "
+        "INNER JOIN states ON cities.state_id = states.id "
         "ORDER BY cities.id ASC"
     )
     query_rows = cursor.fetchall()
